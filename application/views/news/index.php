9d8873bea7f9805e07841f1931551dce6a63652c
Vlad Starshinov <54811872+vladistarshinov@users.noreply.github.com>
2020-01-14 16:31:15 +0300

    Add Friendly URL and references of news

--- application/views/news/index.php
+++ application/views/news/index.php
@@ -1,5 +1,5 @@
 <h1>Все новости</h1>
 
 <?php foreach ($news as $key => $value): ?>
-    <h2> <?php echo $value['title']; ?> </h2>
+    <h2> <a href="view/<?php echo $value['slug']; ?>"> <?php echo $value['title']; ?> </a> </h2>
 <?php endforeach ?>
