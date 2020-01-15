8fd93e3a9176bd106f67550fb94552fff813a25d
Vlad Starshinov <54811872+vladistarshinov@users.noreply.github.com>
2020-01-15 13:13:38 +0300

    Add PSD2HTML Bootstrap into CI Code

--- application/views/templates/footer.php
+++ application/views/templates/footer.php
@@ -1,2 +1,112 @@
+					<!-- Footer start -->
+
+                    </div>
+
+<!-- MENU start -->
+
+<div class="col-lg-3 col-lg-pull-9"> <!-- Меняем блоки местами col-lg-pull-9 -->
+    <div class="panel panel-info hidden-xs">
+        <div class="panel-heading">
+            <div class="sidebar-header">Поиск</div>
+        </div>
+        <div class="panel-body">
+            <form role="search">
+                <div class="form-group">
+                    <div class="input-group">
+                        <input type="search" class="form-control input-lg" placeholder="Ваш запрос">
+                        <div class="input-group-btn">
+                            <button class="btn btn-default btn-lg" type="submit">
+                                <i class="glyphicon glyphicon-search"></i>
+                            </button>
+                        </div>
+                    </div>
+                </div>
+            </form>
+        </div>
+    </div>
+
+    <div class="panel panel-info">
+        <div class="panel-heading">
+            <div class="sidebar-header">Авторизация</div>
+        </div>
+        <div class="panel-body">
+            <form role="form">
+                <div class="form-group">
+                    <input type="text" class="form-control input-lg" placeholder="Логин">
+                </div>
+                <div class="form-group">
+                    <input type="password" class="form-control input-lg" placeholder="Пароль">
+                </div>
+                <button type="submit" class="btn btn-warning pull-right">Вход</button>
+            </form>
+        </div>
+    </div>
+
+    <div class="panel panel-info">
+        <div class="panel-heading">
+            <div class="sidebar-header">Новости</div>
+        </div>
+        <div class="panel-body">
+            <p>31.11.2019</p>
+            <p>Мы запустили новый сервис</p>
+            <button type="submit" class="btn btn-warning pull-right">Читать</button>
+        </div>
+    </div>	
+
+    <div class="panel panel-info">
+        <div class="panel-heading">
+            <div class="sidebar-header">Рейтинг фильмов</div>
+        </div>
+        <div class="panel-body">
+            <ul class="list-group">
+                
+                <li class="list-group-item list-group-warning">
+                    <span class="badge">8.3</span>
+                    <a href="#">Интерстеллар</a>
+                </li>
+
+                <li class="list-group-item list-group-warning">
+                    <span class="badge">8.1</span>
+                    <a href="#">Матрица</a>
+                </li>
+
+                <li class="list-group-item list-group-warning">
+                    <span class="badge">7.6</span>
+                    <a href="#">Безумный Макс</a>
+                </li>
+
+                <li class="list-group-item list-group-warning">
+                    <span class="badge">7.2</span>
+                    <a href="#">Облачный атлас</a>
+                </li>
+                
+            </ul>
+        </div>
+    </div>	
+
+</div>
+
+<!-- MENU end -->
+
+</div>
+</div>
+
+<div class="clear">
+
+</div>
+</div>
+
+<footer>
+<div class="container">
+<p class="text-center"><a href="http://ru.wh-db.com">wh-db.com</a></p>
+</div>
+</footer>
+
+<!-- Footer end -->
+
+<!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+<!-- Include all compiled plugins (below), or include individual files as needed -->
+<script src="/assets/js/bootstrap.min.js"></script>
 </body>
 </html>
