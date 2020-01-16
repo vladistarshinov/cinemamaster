e57af8db7bcdaeb0b69db6f0bac008197d43ca0e
Vlad Starshinov <54811872+vladistarshinov@users.noreply.github.com>
2020-01-16 23:45:21 +0300

    Remove double folder application from Application

--- application/application/views/news/index.php
+++
@@ -1,9 +0,0 @@
-<h1>Все новости</h1>
-
-<?php foreach ($news as $key => $value): ?>
-	<p><a href="view/<?php echo $value['slug']; ?>"><?php echo $value['title']; ?></a> | 
-	<a href="edit/<?php echo $value['slug']; ?>">Изменить</a> | 
-	<a href="delete/<?php echo $value['slug']; ?>">Удалить</a></p>
-<?php endforeach ?>
-
-<p><a href="create">Добавить новость</a></p><br>
